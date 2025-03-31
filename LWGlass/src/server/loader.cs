0fe73618879c97196c93a957fe245c65335bcf8a
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-03-30 20:27:13 -0400

    Delete LWGlass directory

--- LWGlass/src/server/loader.cs
+++
@@ -1,8 +0,0 @@
-﻿using LogicAPI.Server;
-using LogicLog;
-
-public class Loader : ServerMod {
-    protected override void Initialize() {
-        Logger.Info("LWGServer - Loaded server");
-    }
-}
